fb6cfcadf975d9c448e6721a59f74b029e8f0cf7
Nyasha Shonhiwa <nyasha.shonhiwa@zccinzim.onmicrosoft.com>
2026-05-06 15:50:11 +0200

    Revert "Modernize dashboard UI with sidebar shell and Tailwind foundations"
    This reverts commit 997ebb3dd3800cb4c031e1f4daf2c62b6dd1156c.

--- client/src/components/ui/label.jsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '../../lib/utils'
-
-function Label({ className, ...props }) {
-  return <label className={cn('text-sm font-medium text-slate-700', className)} {...props} />
-}
-
-export { Label }
-
--- client/src/store/uiStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-export const useUiStore = create((set) => ({
-  activeRoute: 'overview',
-  sidebarOpen: false,
-  setActiveRoute: (activeRoute) => set({ activeRoute }),
-  setSidebarOpen: (sidebarOpen) => set({ sidebarOpen }),
-}))
-
